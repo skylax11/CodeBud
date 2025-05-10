4ac6ecd200004922a31d56cf23b9d935d7609b9f
skylax11 <devranpvp683@gmail.com>
2025-05-10 18:40:56 +0300

    Add all entity models

---
+++ CodeBud/Models/Entities/Question.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeBud.Models.Entities
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<Answer> Answers { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public ICollection<QuestionTag> QuestionTags { get; set; }
+        public ICollection<Vote> Votes { get; set; }
+    }
+}
---
+++ CodeBud/Models/Entities/Vote.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeBud.Models.Entities
+{
+    public class Vote
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public bool IsUpvote { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [ForeignKey("Question")]
+        public int? QuestionId { get; set; }
+        public Question Question { get; set; }
+
+        [ForeignKey("Answer")]
+        public int? AnswerId { get; set; }
+        public Answer Answer { get; set; }
+    }
+}
