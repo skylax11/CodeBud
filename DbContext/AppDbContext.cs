964b3ae065e00805a6411c0f0534933acadb3234
ErenReyhanlioglu <116086022+ErenReyhanlioglu@users.noreply.github.com>
2025-05-11 16:56:42 +0300

    Add files via upload

---
+++ DbContext/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CodeBud.Models;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeBud.DbContext
+{
+    public class AppDbContext : System.Data.Entity.DbContext
+    {
+        public AppDbContext() : base("DefaultConnection") { }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
