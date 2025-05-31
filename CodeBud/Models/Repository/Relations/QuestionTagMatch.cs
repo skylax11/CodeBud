4afd074767b2be5f6f1cbe986f0a12bae6fa78e0
Devran Yılmaz <117036699+skylax11@users.noreply.github.com>
2025-05-31 18:54:07 +0300

    Question ile tag eşleştirmeyi sağlayan yapı

---
+++ CodeBud/Models/Repository/Relations/QuestionTagMatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeBud.Models.Repository.Relations
+{
+    public class QuestionTagMatch
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int QuestionId { get; set; }
+        [Required]
+        public int TagId { get; set; }
+    }
+}
