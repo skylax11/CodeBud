4c49a4edfce2a7fea56d77a78826c06cfd9d8f89
Devran Yılmaz <117036699+skylax11@users.noreply.github.com>
2025-05-31 18:53:04 +0300

    Tag added

---
+++ CodeBud/Models/Entities/Tag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeBud.Models.Entities
+{
+    public class Tag
+    {
+        [Key]
+        public int TagId { get; set; }
+
+        [Required]
+        public string TagName { get; set; }
+
+    }
+}
